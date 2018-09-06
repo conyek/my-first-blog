94f077115a13176c5cd5a5fab6246e4942799990
kimeunkyung <lovekei@gmail.com>
2018-09-06 16:30:09 +0800

    updated navigation

---
+++ blog/static/js/scroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#link1").click(function() {
+    $path = $("#whatIdo").offset().top;
+    $("body").animate({ scrollTop: $path }, 1000);
+  });
+  $("#link2").click(function() {
+    $path = $("#work").offset().top;
+    $("body").animate({ scrollTop: $path }, 1000);
+  });
+  $("#link3").click(function() {
+    $path = $("#contact").offset().top;
+    $("body").animate({ scrollTop: $path }, 1000);
+  });
+});
